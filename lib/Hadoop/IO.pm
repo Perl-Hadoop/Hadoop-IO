56fc88a6a4a30c5aa368435c8469d960e8c8e915
Burak Gürsoy <burak@cpan.org>
2023-06-23 17:32:12 +0200

    Workaround PAUSE

---
+++ lib/Hadoop/IO.pm
@@ -0,0 +1,21 @@
+package Hadoop::IO;
+
+use 5.010;
+use strict;
+use warnings;
+
+1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Hadoop::IO - This is not a real code and only purpose is to keep PAUSE happy
+
+=head1 SYNOPSIS
+
+None
+
+=cut
